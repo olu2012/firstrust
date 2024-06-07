c1c3298e4c67f7a7174527b1795e25b3d4c356f2
israel <ioluyomi@gmail.com>
2024-06-07 11:45:13 +0100

    first commit

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let name="jobtut";
+    println!("Hello, {}!", name);
+    println!("Hello, world!");
+    let age=20;
+    match age {
+        10 => println!("10"),
+        20 => println!("20"),
+        30 => println!("30"),
+        _ => println!("other"),
+    }
+}
